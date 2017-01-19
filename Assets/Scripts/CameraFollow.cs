5b1ea829024f756fdf85da506b9302e2887a0996
Tung_Nguyen <7319tnguyen040@gmail.com>
2017-01-18 17:51:09 -0800

    Almost finished camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -9,9 +9,13 @@ public class CameraFollow : MonoBehaviour
     private Vector3 Velo = Vector3.zero;
     private Vector3 CamTargetDifference;
 
-    private float boop = 0;
-    private float breakpoint = int.MaxValue;
-    private bool Augoo = true;
+    public float ScrollSpeed = 5;
+    public float ZoomSpeed = 3;
+    public float ZoomRate = 0.5f;
+    public float ZoomMin = -10;
+    public float ZoomMax = -3;
+    public float ZoomForward = 15;
+    private float newMouseZ = 0;
 
     public GameObject Player;
 
@@ -19,28 +23,55 @@ public class CameraFollow : MonoBehaviour
     {
         CamTargetDifference = transform.position - Player.transform.position;
         transform.LookAt(Player.transform);
+        newMouseZ = transform.position.z;
     }
 
     void FixedUpdate()
     {
+        MouseLock();
+        MouseLook();
+        MouseZoom();
+    }
+
+    void MouseLock()
+    {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        //Camera Follow
+    }
+
+    void MouseLook()
+    {
         transform.RotateAround(new Vector3(Player.transform.position.x, Player.transform.position.y, Player.transform.position.z), new Vector3(0, 0, 1), -Input.GetAxis("Mouse X") * 2);
-        boop += Input.GetAxis("Mouse Y") * 0.5f;
         transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0) * 2);
-        transform.position += new Vector3(0, 0, Input.GetAxis("Mouse Y") * 0.5f);
-        if (transform.position.z >= -3)
+    }
+
+    void MouseZoom()
+    {
+        /*Zoom*/
+        //back
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -3);
-            if(Augoo == true)
-            {
-                breakpoint = boop;
-                Augoo = false;
-            }
+            newMouseZ += ZoomRate;
+            if (newMouseZ > ZoomMax) newMouseZ = ZoomMax;
+        }
+        //forward
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            newMouseZ -= ZoomRate;
+            if (newMouseZ < ZoomMin) newMouseZ = ZoomMin;
         }
 
-        Debug.Log("Sheet: " + boop);
-        Debug.Log("Break: " + breakpoint);
+        float normalizedMouseZ = (newMouseZ - ZoomMin) / (ZoomMax - ZoomMin);
+        Vector3 playerDirection = (transform.position - Player.transform.position).normalized;
+
+        if(playerDirection.x == 0 && playerDirection.y == 0 && newMouseZ != normalizedMouseZ)
+        {
+            playerDirection = -Player.transform.up;
+        }
+
+        Debug.Log("MOO: " + normalizedMouseZ);
+        Debug.Log("X: " + playerDirection.x + "Y: " + playerDirection.y);
+
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(Player.transform.position.x + (playerDirection.x * ZoomForward * normalizedMouseZ), Player.transform.position.y + (playerDirection.y * ZoomForward * normalizedMouseZ), newMouseZ), ScrollSpeed * Time.deltaTime);
     }
 }
